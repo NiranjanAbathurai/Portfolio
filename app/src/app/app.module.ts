96b05edf32903481f5834e49ae1eaf3228133587
Niranjan <niranjanhari464@gmail.com>
2025-03-28 17:46:32 +0530

    animation changes

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -5,8 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
-import { AboutComponent } from './about/about.component';
-import { ProjectsComponent } from './projects/projects.component';
 import { ContactComponent } from './contact/contact.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { FooterComponent } from './footer/footer.component';
@@ -19,8 +17,6 @@ import { ReactiveFormsModule } from '@angular/forms';
     AppComponent,
     HomeComponent,
     HeaderComponent,
-    AboutComponent,
-    ProjectsComponent,
     ContactComponent,
     PageNotFoundComponent,
     FooterComponent,
